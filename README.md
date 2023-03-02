9ba6ddefe24bd3fd19c9ed82e681de5ad58bd9b9
Taras Basiuk <BasiukTV@gmail.com>
2023-03-02 02:45:39 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# How To Use
+* Just load the whole folder unpacked into Chrome Extensions (Developer mode has to be On).
+
+## Disclaimers
+* This worked as of 03/02/2023, but will probably eventually break.
+* This extension is not particularly efficient, I only spent a few hours on this and generally not that good at JS.
---
+++ scripts/content.js
@@ -0,0 +1,59 @@
+// We set a timer to periodically check if the subscription feed browser is ready.
+const timerId = setInterval(() => {
+
+    // We check if the feed browser is ready
+    const feedBrowser = document.getElementsByTagName('ytd-browse')[0];
+    if (!feedBrowser) {
+        // Not ready yet, will try next time.
+        return;
+    }
+
+    // If the feed browser is ready, no need to look for it anymore, so we clear the timer.
+    clearInterval(timerId);
+
+    // Next we will setup the mutation observer on the feed browser
+
+    // Observer should be configured to react to child mutations anywhere down the subtree.
+    const observerConfig = { attributes: false, childList: true, subtree: true };
+
+    /*
+        Callback when feedBrowser children change, which is admittedly pretty noisy, but,
+        hopefully YouTube subscription feed is not a very dynamic page.
+    */
+    const mutationCallback = (mutationList, observer) => {
+
+        // Look for SHORTS overlay icons
+        feedBrowser.querySelectorAll('[overlay-style="SHORTS"]').forEach((e) => {
+
+            // We'll have to climb four parent elements before we can tell which layout is used.
+            for (var i = 0; i < 4; i++) {
+                e = e.parentElement;
+            }
+
+            // Presence of ytd-grid-video-renderer CSS class indicates the Grid layout.
+            const isGridLayout = e.classList.contains("ytd-grid-video-renderer");
+
+            /* 
+                If grid layout is used - we need to climb one parent until we get to subscription feed item.
+                If list layout is used - we need to climb five parents for the same.
+            */
+            for (var i = 0; i < (isGridLayout ? 1 : 5); i++) {
+                e = e.parentElement;
+            }
+
+            // If this feed item is not hidden yet, hide it.
+            const hiddenAlready = e.style.display === 'none';
+            if (!hiddenAlready) {
+                e.style.display = 'none';
+            }
+        });
+    };
+
+    // Execute the callback once even before any items change, for the initial removal of shorts.
+    mutationCallback(null, null);
+
+    // Initialize and activate the observer.
+    const observer = new MutationObserver(mutationCallback);
+    observer.observe(feedBrowser, observerConfig);
+
+}, 1000); // Timer will activate once every second.
